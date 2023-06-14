4f6ec7c9d06cd7f2be71acd3c26c0927abb35fe6
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-06-15 00:25:20 +0700

    Create Servcie

--- hooks/useFormatCurrency.js
+++ hooks/useFormatCurrency.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useFormatCurrency = (amount) => {
     const [formattedAmount, setFormattedAmount] = useState('');
 
-    const formatCurrency = (number) => {
-        const formatter = new Intl.NumberFormat('vi-VN', {
-            style: 'currency',
-            currency: 'VND',
-        });
-        return formatter.format(number);
-    };
+    useEffect(() => {
+        const formatCurrency = (number) => {
+            const formatter = new Intl.NumberFormat('vi-VN', {
+                style: 'currency',
+                currency: 'VND',
+            });
+            return formatter.format(number);
+        };
 
-    const cleanedAmount = amount.replace(/[^\d]/g, '');
-    const formattedNumber = formatCurrency(cleanedAmount);
-    setFormattedAmount(formattedNumber);
+        const cleanedAmount = amount.replace(/[^\d]/g, '');
+        const formattedNumber = formatCurrency(cleanedAmount);
+        setFormattedAmount(formattedNumber);
+    }, [amount]);
 
-    return formattedNumber;
+    return formattedAmount;
 };
 
 export default useFormatCurrency;
